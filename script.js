f9189e6e2933a6b75ce7bd83e1abd2fbb5bbd371
andymarco145 <tduc1405@gmail.com>
2024-08-05 16:17:36 +0700

    first upload

---
+++ script.js
@@ -0,0 +1,13 @@
+function showMessage() {
+    var heart = document.getElementById("heart");
+    var messageCard = document.getElementById("messageCard");
+    var button = document.querySelector(".press-btn");
+    
+    button.style.display = "none"; // Ẩn nút
+    
+    heart.style.display = "block";
+    setTimeout(function() {
+        heart.style.display = "none";
+        messageCard.style.transform = "translate(-50%, -50%) scale(1)";
+    }, 10000); // Hiệu ứng trái tim động và di chuyển trong 10 giây
+}
